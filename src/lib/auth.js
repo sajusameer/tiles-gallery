49d063e5e67a83fc7697ea25d819b5426e7b3ac2
sajusameer <sajudsameer32@gmail.com>
2026-05-03 01:15:50 +0600

    login and register page added

---
+++ src/lib/auth.js
@@ -0,0 +1,28 @@
+import dns from "node:dns";
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+let db;
+
+async function connectDB() {
+  if (!db) {
+    await client.connect();
+    db = client.db();
+  }
+  return db;
+}
+
+export const auth = betterAuth({
+  database: mongodbAdapter(await connectDB(), {
+    client,
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+});
