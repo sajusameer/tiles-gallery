6b4f3441c76cb093b12f869b2711b1b43cf1b3e3
sajusameer <sajudsameer32@gmail.com>
2026-05-03 03:09:44 +0600

    photo added

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,10 +23,11 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-    <Navbar/>
+        <Navbar/>
         {children}
         <Footer/>
         </body>
